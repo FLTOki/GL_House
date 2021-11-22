4026f2934358122278cc739ff1d7bc1a23727638
FLTOki <jim.musyoki@strathmore.edu>
2021-11-22 09:40:19 +0300

    hut display

--- house/main.cpp
+++ house/main.cpp
@@ -1,177 +1,191 @@
-git/*
- * GLUT Shapes Demo
- *
- * Written by Nigel Stewart November 2003
- *
- * This program is test harness for the sphere, cone
- * and torus shapes in GLUT.
- *
- * Spinning wireframe and smooth shaded shapes are
- * displayed until the ESC or q key is pressed.  The
- * number of geometry stacks and slices can be adjusted
- * using the + and - keys.
- */
+// C++ program for the above approach
 
-#ifdef __APPLE__
-#include <GLUT/glut.h>
-#else
-#include <GL/glut.h>
-#endif
+#include <GL\glut.h>
+#include <iostream>
+#include <windows.h>
+using namespace std;
 
-#include <stdlib.h>
-
-static int slices = 16;
-static int stacks = 16;
-
-/* GLUT callback Handlers */
-
-static void resize(int width, int height)
+// Function to initialize the drivers
+void myInit(void)
 {
-    const float ar = (float) width / (float) height;
+    // Clear all the screen color
+    glClearColor(1.0, 0.5, 0.0, 1.0);
 
-    glViewport(0, 0, width, height);
+    // Sets background color to orang
     glMatrixMode(GL_PROJECTION);
+
     glLoadIdentity();
-    glFrustum(-ar, ar, -1.0, 1.0, 2.0, 100.0);
 
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity() ;
+    // Specify the display area
+    gluOrtho2D(0.0, 400.0, 0.0, 400.0);
 }
 
-static void display(void)
+// Function to display the Hut like
+// structure on the console
+void myDisplay(void)
 {
-    const double t = glutGet(GLUT_ELAPSED_TIME) / 1000.0;
-    const double a = t*90.0;
+    // Clear the screen buffer
+    glClear(GL_COLOR_BUFFER_BIT);
 
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glColor3d(1,0,0);
+    glPointSize(4.0);
 
-    glPushMatrix();
-        glTranslated(-2.4,1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutSolidSphere(1,slices,stacks);
-    glPopMatrix();
+    // Rectangular part of hut
+    glColor3f(0.5f, 0.5f, 0.5f);
 
-    glPushMatrix();
-        glTranslated(0,1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutSolidCone(1,1,slices,stacks);
-    glPopMatrix();
+    // Begin the polygon
+    glBegin(GL_POLYGON);
 
-    glPushMatrix();
-        glTranslated(2.4,1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutSolidTorus(0.2,0.8,slices,stacks);
-    glPopMatrix();
+    // Create the polygon
+    glVertex2i(40, 40);
+    glVertex2i(320, 40);
 
-    glPushMatrix();
-        glTranslated(-2.4,-1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutWireSphere(1,slices,stacks);
-    glPopMatrix();
+    glVertex2i(40, 200);
+    glVertex2i(320, 200);
 
-    glPushMatrix();
-        glTranslated(0,-1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutWireCone(1,1,slices,stacks);
-    glPopMatrix();
+    glVertex2i(40, 200);
+    glVertex2i(40, 40);
 
-    glPushMatrix();
-        glTranslated(2.4,-1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutWireTorus(0.2,0.8,slices,stacks);
-    glPopMatrix();
+    glVertex2i(320, 200);
+    glVertex2i(320, 40);
 
-    glutSwapBuffers();
+    glEnd();
+
+    // Right Window Update
+    glColor3f(1.0f, 0.0f, 0.0f);
+
+    // Begin the polygon
+    glBegin(GL_POLYGON);
+
+    // Create the polygon
+    glVertex2i(220, 60);
+    glVertex2i(300, 60);
+
+    glVertex2i(220, 150);
+    glVertex2i(300, 150);
+
+    glVertex2i(220, 60);
+    glVertex2i(220, 150);
+
+    glVertex2i(300, 150);
+    glVertex2i(300, 60);
+
+    glEnd();
+
+    // Right Window Update part 2
+    glColor3f(1.0f, 0.0f, 0.0f);
+
+    // Begin the polygon
+    glBegin(GL_POLYGON);
+
+    // Create the polygon
+    glVertex2i(220, 170);
+    glVertex2i(300, 170);
+
+    glVertex2i(220, 190);
+    glVertex2i(300, 190);
+
+    glVertex2i(220, 170);
+    glVertex2i(220, 190);
+
+    glVertex2i(300, 190);
+    glVertex2i(300, 170);
+
+    glEnd();
+
+    // Door
+    glColor3f(0.60f, 0.42f, 0.16f);
+
+    // Begin the polygon
+    glBegin(GL_POLYGON);
+
+    // Create the polygon
+    glVertex2i(130, 40);
+    glVertex2i(130, 160);
+
+    glVertex2i(130, 160);
+    glVertex2i(180, 160);
+
+    glVertex2i(180, 100);
+    glVertex2i(180, 40);
+
+    glVertex2i(120, 40);
+    glVertex2i(170, 40);
+
+    glEnd();
+
+    // Create Door Part 2
+    glColor3f(0.60f, 0.42f, 0.16f);
+
+    // Begin the polygon
+    glBegin(GL_POLYGON);
+
+    // Create the polygon
+    glVertex2i(130, 170);
+    glVertex2i(130, 180);
+
+    glVertex2i(130, 180);
+    glVertex2i(180, 180);
+
+    glVertex2i(180, 170);
+    glVertex2i(180, 180);
+
+    glVertex2i(130, 170);
+    glVertex2i(180, 170);
+
+    glEnd();
+
+    // Hut's top triangle part
+    glColor3f(1.0f, 0.0f, 1.0f);
+
+    // Begin the polygon
+    glBegin(GL_POLYGON);
+
+    // Create the polygon
+    glVertex2i(10, 200);
+    glVertex2i(340, 200);
+
+    glVertex2i(200, 390);
+
+    glVertex2i(10, 200);
+    glVertex2i(200, 390);
+
+    glEnd();
+
+    // Sends all output to display
+    glFlush();
 }
 
-
-static void key(unsigned char key, int x, int y)
+// Driver Code
+int main(int argc, char **argv)
 {
-    switch (key)
-    {
-        case 27 :
-        case 'q':
-            exit(0);
-            break;
-
-        case '+':
-            slices++;
-            stacks++;
-            break;
-
-        case '-':
-            if (slices>3 && stacks>3)
-            {
-                slices--;
-                stacks--;
-            }
-            break;
-    }
-
-    glutPostRedisplay();
-}
-
-static void idle(void)
-{
-    glutPostRedisplay();
-}
-
-const GLfloat light_ambient[]  = { 0.0f, 0.0f, 0.0f, 1.0f };
-const GLfloat light_diffuse[]  = { 1.0f, 1.0f, 1.0f, 1.0f };
-const GLfloat light_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
-const GLfloat light_position[] = { 2.0f, 5.0f, 5.0f, 0.0f };
-
-const GLfloat mat_ambient[]    = { 0.7f, 0.7f, 0.7f, 1.0f };
-const GLfloat mat_diffuse[]    = { 0.8f, 0.8f, 0.8f, 1.0f };
-const GLfloat mat_specular[]   = { 1.0f, 1.0f, 1.0f, 1.0f };
-const GLfloat high_shininess[] = { 100.0f };
-
-/* Program entry point */
-
-int main(int argc, char *argv[])
-{
+    // Initialize the init function
     glutInit(&argc, argv);
-    glutInitWindowSize(640,480);
-    glutInitWindowPosition(10,10);
-    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
 
-    glutCreateWindow("GLUT Shapes");
+    // Initialize the toolkit;
+    glutInitDisplayMode(
+        GLUT_SINGLE | GLUT_RGB);
 
-    glutReshapeFunc(resize);
-    glutDisplayFunc(display);
-    glutKeyboardFunc(key);
-    glutIdleFunc(idle);
+    // Sets the display mode and
+    // specify the colour scheme
+    glutInitWindowSize(1200, 740);
 
-    glClearColor(1,1,1,1);
-    glEnable(GL_CULL_FACE);
-    glCullFace(GL_BACK);
+    // Specify the window size
+    glutInitWindowPosition(0, 0);
 
-    glEnable(GL_DEPTH_TEST);
-    glDepthFunc(GL_LESS);
+    // Sets the starting position
+    // for the window
+    glutCreateWindow("Basic hut like"
+                     " structure");
 
-    glEnable(GL_LIGHT0);
-    glEnable(GL_NORMALIZE);
-    glEnable(GL_COLOR_MATERIAL);
-    glEnable(GL_LIGHTING);
+    // Creates the window and
+    // sets the title
+    glutDisplayFunc(myDisplay);
+    myInit();
 
-    glLightfv(GL_LIGHT0, GL_AMBIENT,  light_ambient);
-    glLightfv(GL_LIGHT0, GL_DIFFUSE,  light_diffuse);
-    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
-    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
-
-    glMaterialfv(GL_FRONT, GL_AMBIENT,   mat_ambient);
-    glMaterialfv(GL_FRONT, GL_DIFFUSE,   mat_diffuse);
-    glMaterialfv(GL_FRONT, GL_SPECULAR,  mat_specular);
-    glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess);
-
+    // Additional initializations
     glutMainLoop();
 
-    return EXIT_SUCCESS;
+    // Go into a loop until event
+    // occurs
+    return 0;
 }
